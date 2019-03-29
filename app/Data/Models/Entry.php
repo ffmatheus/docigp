82e8c04abacc77baec5ecd728aa231a329291569
BMasquio <brunomasquio@gmail.com>
2019-03-29 11:37:24 -0300

    Create entries table

---
+++ app/Data/Models/Entry.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Data\Models;
+
+class Entry extends Model
+{
+    protected $table = 'entries';
+
+    /**
+     * @var array
+     */
+    protected $fillable = [
+        'date',
+        'value',
+        'object',
+        'to',
+        'cycle_id',
+        'verified_at',
+        'authorised_at',
+        'published_at',
+        'verified_by_id',
+        'authorised_by_id',
+        'published_by_id',
+        'created_by_id',
+        'updated_by_id',
+    ];
+
+    protected $dates = ['date', 'verified_at', 'authorised_at', 'published_at'];
+}
