18802c94fb557da26dd7226e3012a722b7e1c8b7
BMasquio <brunomasquio@gmail.com>
2019-03-29 11:20:48 -0300

    Create cycles table

---
+++ app/Data/Models/Cycle.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Data\Models;
+
+class Cycle extends Model
+{
+    /**
+     * @var array
+     */
+    protected $fillable = [
+        'congressman_legislature_id',
+        'year',
+        'month',
+        'published_at',
+        'published_by_id',
+        'created_by_id',
+        'updated_by_id',
+    ];
+}
