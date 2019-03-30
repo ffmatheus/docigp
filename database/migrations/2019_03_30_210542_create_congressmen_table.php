50168297b9d0d755b66e328c55108c1a45b4ed54
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2019-03-30 18:12:36 -0300

    Add DataSyncService

---
+++ database/migrations/2019_03_30_210542_create_congressmen_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCongressmenTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('congressmen', function (Blueprint $table) {
+            $table->bigIncrements('id');
+
+            $table->bigIncrements('remote_id')->unsigned();
+            $table->string('name');
+            $table->string('nickname');
+            $table->bigIncrements('party_id')->unsigned();
+            $table->string('nickname');
+            $table->string('photo_url');
+            $table->string('thumbnail_url');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('congressmen');
+    }
+}
