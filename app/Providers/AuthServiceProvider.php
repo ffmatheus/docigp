ac0bd94f08b7e414f616ec081f2b1f01becd2560
BMasquio <brunomasquio@gmail.com>
2019-04-01 15:35:46 -0300

    Making users crud page

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -25,6 +25,8 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        //
+        Gate::define('appUser', function ($user) {
+            return true;
+        });
     }
 }
--- routes/auth/web/users.php
+++ routes/auth/web/users.php
@@ -1,4 +1,8 @@
 <?php
 Route::group(['prefix' => '/users'], function () {
     Route::get('/', 'Users@index')->name('users.index');
+
+    Route::post('/', 'Users@store')->name('users.store');
+
+    Route::get('/{id}', 'Users@show')->name('users.show');
 });
