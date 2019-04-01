03986ec48c21b94c66d415f7e5304402330ed775
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2019-04-01 14:28:10 -0300

    Rename commands

--- routes/console.php
+++ routes/console.php
@@ -2,10 +2,10 @@
 
 use App\Services\DataSync\Service as DataSyncService;
 
-Artisan::command('vg:sync:congressmen', function () {
+Artisan::command('docigp:sync:congressmen', function () {
     app(DataSyncService::class)->congressmen();
 })->describe('Sync congressmen data');
 
-Artisan::command('vg:sync:parties', function () {
+Artisan::command('docigp:sync:parties', function () {
     app(DataSyncService::class)->parties();
 })->describe('Sync congressmen data');
