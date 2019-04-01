69a4cac5645e9ac6f80da955c368695121121295
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2019-04-01 14:29:05 -0300

    Add utility buttons

--- resources/views/admin/home/index.blade.php
+++ resources/views/admin/home/index.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-10">
             <div class="card">
-                <div class="card-header">Dashboard</div>
+                <div class="card-header">Painel de Controle</div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -14,7 +14,24 @@
                         </div>
                     @endif
 
-                    You are logged in!
+                    <a class="btn btn-primary btn-lg p-5 m-lg-5" href="{{ route('entries.index') }}">
+                        Prestação de Contas
+                    </a>
+                    <a class="btn btn-primary btn-lg p-5 m-lg-5" href="{{ route('cycles.index') }}">
+                        Ciclos
+                    </a>
+                    <a class="btn btn-primary btn-lg p-5 m-lg-5" href="{{ route('parties.index') }}">
+                        Partidos
+                    </a>
+                    <a class="btn btn-primary btn-lg p-5 m-lg-5" href="{{ route('legislatures.index') }}">
+                        Legislaturas
+                    </a>
+                    <a class="btn btn-primary btn-lg p-5 m-lg-5" href="{{ route('congressmen.index') }}">
+                        Deputados
+                    </a>
+                    <a class="btn btn-primary btn-lg p-5 m-lg-5" href="{{ route('users.index') }}">
+                        Usuários
+                    </a>
                 </div>
             </div>
         </div>
