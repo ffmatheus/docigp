5ad2228f9a2fba64a38d243c2350badb0642a3ef
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2019-04-02 00:50:48 -0300

    Add view composers for environment

---
+++ app/Http/ViewComposers/GlobalComposer.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\ViewComposers;
+
+use Illuminate\View\View;
+use App\Services\Environment\Service as Environment;
+
+class GlobalComposer
+{
+    /**
+     * Bind data to the view.
+     *
+     * @param View $view
+     * @return void
+     * @throws \Illuminate\Contracts\Container\BindingResolutionException
+     */
+    public function compose(View $view)
+    {
+        $view->with('environment', $this->getEnvironmentService());
+
+        $view->with('app_name', config('app.name'));
+    }
+
+    /**
+     * @return mixed
+     * @throws \Illuminate\Contracts\Container\BindingResolutionException
+     */
+    private function getEnvironmentService()
+    {
+        if (app()->has(Environment::class)) {
+            return app()->make(Environment::class);
+        }
+
+        $environment = app(Environment::class)->data();
+
+        app()->singleton(Environment::class, function ($app) use (
+            $environment
+        ) {
+            return $environment;
+        });
+
+        return $environment;
+    }
+}
---
+++ app/Services/Environment/Service.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Services\Environment;
+
+use PragmaRX\Version\Package\Facade as Version;
+
+class Service
+{
+    public function data()
+    {
+        return coollect([
+            'app' => [
+                'name' => config('app.name'),
+                'key' => auth()->user()
+                    ? auth()->user()->client_key
+                    : config('app.client_key'),
+                'id' => auth()->user() ? auth()->user()->email : csrf_token(),
+                'version' => Version::format('compact'),
+            ],
+
+            'token' => csrf_token(),
+
+            'captcha_enabled' =>
+                app()->environment() == 'local' && env('DISABLE_CAPTCHA')
+                    ? false
+                    : true,
+
+            'captcha_siteKey' => config('services.recaptcha.siteKey'),
+
+            'debug' => config('app.debug'),
+
+            'session' => [
+                'lifetime' => config('session.lifetime'),
+
+                'frontend' => [
+                    'lifetime' => config('session.frontend.lifetime'),
+
+                    'warning' => config('session.frontend.warning'),
+                ],
+            ],
+
+            'user' => dd(auth()->user()),
+
+            'timestamp' => now()->toDateTimeString(),
+
+            'old' => coollect(old())
+                ->except('password', 'password_confirmation')
+                ->toArray(),
+
+            'events' => [
+                'confirmation' => [
+                    'enabled' => config('events.confirmation.enabled'),
+                ],
+            ],
+
+            'broadcast' => [
+                'driver' => config('broadcasting.default', 'pusher'),
+            ],
+
+            'pusher' => [
+                'server' => config('broadcasting.connections.pusher.server'),
+
+                'key' => config('broadcasting.connections.pusher.key'),
+
+                'options' => [
+                    'cluster' => config(
+                        'broadcasting.connections.pusher.options.cluster',
+                        'us2'
+                    ),
+                    'encrypted' => config(
+                        'broadcasting.connections.pusher.options.encrypted',
+                        false
+                    ),
+                    'host' => config(
+                        'broadcasting.connections.pusher.options.backend_host',
+                        '127.0.0.1'
+                    ),
+                    'port' => config(
+                        'broadcasting.connections.pusher.options.port',
+                        '6001'
+                    ),
+                    'scheme' => config(
+                        'broadcasting.connections.pusher.options.scheme',
+                        'http'
+                    ),
+                ],
+            ],
+        ]);
+    }
+}
