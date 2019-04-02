861bf56098f3992e4b4671a87e5944af1bf6aa7c
Orlando Vinícius <orlando.vinicius1987@gmail.com>
2019-04-02 16:40:03 +0000

    Issue: #1

--- app/Http/Controllers/Web/Admin/Congressmen.php
+++ app/Http/Controllers/Web/Admin/Congressmen.php
@@ -3,7 +3,37 @@
 namespace App\Http\Controllers\Web\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Data\Repositories\Congressmen as CongressmenRepository;
 
 class Congressmen extends Controller
 {
+
+    private $repository;
+
+    public function __construct(CongressmenRepository $repository)
+    {
+        $this->repository = $repository;
+    }
+
+
+    public function index()
+    {
+        return view('admin.congressmen.index');
+    }
+
+    public function create()
+    {
+        return view('admin.congressmen.form')->with([
+            'congressman' => $this->repository->new(),
+        ]);
+    }
+
+    public function store(
+        CongressmanRequest $request,
+        CongressmenRepository $repository
+    ) {
+        $repository->createFromRequest($request);
+
+        return redirect()->route('admin.congressmen.index');
+    }
 }
