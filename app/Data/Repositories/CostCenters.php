d787ade3759f84b6dd114c30346c64f115d5105a
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2019-04-06 11:08:11 -0300

    Add Cost Centers and Providers

---
+++ app/Data/Repositories/CostCenters.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Data\Repositories;
+
+use App\Data\Models\CostCenter;
+
+class CostCenters extends Repository
+{
+    /**
+     * @var string
+     */
+    protected $model = CostCenter::class;
+}
