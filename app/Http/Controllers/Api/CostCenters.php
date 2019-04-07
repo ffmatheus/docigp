4bec0f19ca95f628f7986ef296cde38960f882fa
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2019-04-07 17:40:10 -0300

    Add CostCenter api

---
+++ app/Http/Controllers/Api/CostCenters.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Data\Repositories\CostCenters as CostCentersRepository;
+
+class CostCenters extends Controller
+{
+    /**
+     * Get all data
+     *
+     * @return array
+     */
+    public function all()
+    {
+        return app(CostCentersRepository::class)->all();
+    }
+}
