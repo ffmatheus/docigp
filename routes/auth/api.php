808b7466d8a27181e9a55d2f05fefca090645092
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2019-04-07 17:40:30 -0300

    Add cost and cpf routes

--- routes/auth/api.php
+++ routes/auth/api.php
@@ -9,5 +9,7 @@ Route::group(
         require __DIR__ . '/api/congressmen.php';
         require __DIR__ . '/api/budgets.php';
         require __DIR__ . '/api/users.php';
+        require __DIR__ . '/api/costCenters.php';
+        require __DIR__ . '/api/cpfCnpj.php';
     }
 );
