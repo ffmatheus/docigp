2498b297c5f8f8c1f2e4be6890db37e7977dcda3
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2019-04-08 00:56:35 -0300

    Move home to transparency page

--- routes/no-auth/web/home.php
+++ routes/no-auth/web/home.php
@@ -1,3 +1,7 @@
 <?php
 
 Route::get('/', 'Home@index')->name('home');
+
+Route::get('/', 'Home@index')->name('home.index');
+
+Route::get('/transparencia', 'Home@transparency')->name('home.transparency');
