ee3670d3fb3f3b895db762b08d246315ae20d412
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2019-04-08 23:44:08 -0300

    Add downloadable file

--- resources/views/admin/entries/index.blade.php
+++ resources/views/admin/entries/index.blade.php
@@ -1,7 +1,15 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container-fluid" id="vue-entries">
-    <app-account></app-account>
-</div>
+    <div class="row">
+        <div class="col text-right">
+            <a href="/files/docigp.pdf" class="btn btn-primary btn-sm" download>
+                <i class="fa fa-cloud-download-alt"></i> Baixar Ato Nº 641/2019
+            </a>
+        </div>
+    </div>
+
+    <div class="container-fluid" id="vue-entries">
+        <app-account></app-account>
+    </div>
 @endsection
