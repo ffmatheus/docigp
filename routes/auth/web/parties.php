0833d0d74ad4082c772ab96864d90f94bb1a7fab
Orlando Vinícius <orlando.vinicius1987@gmail.com>
2019-04-16 16:14:30 +0000

    Acertando inclusão de partidos

--- routes/auth/web/parties.php
+++ routes/auth/web/parties.php
@@ -1,6 +1,8 @@
 <?php
 
 Route::group(['prefix' => '/parties'], function () {
+    Route::get('/create', 'Parties@create')->name('parties.create');
+
     Route::get('/', 'Parties@index')->name('parties.index');
 
     Route::get('/{id}', 'Parties@show')->name('parties.show');
