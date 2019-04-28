7be96bffad488861dac51ac6ea90ac568d8ef905
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2019-04-28 20:05:46 -0300

    Fix not being able to open congressmen form

--- resources/views/partials/save-button.blade.php
+++ resources/views/partials/save-button.blade.php
@@ -1,7 +1,16 @@
-<button id="cancelButton" class="btn btn-success ml-1" v-on:click.prevent="cancel()"  :disabled="!(isEditing || isCreating)">
+<button
+    id="cancelButton"
+    class="btn btn-success ml-1"
+    v-on:click.prevent="cancel()"
+    :disabled="!(isEditing || isCreating)"
+>
     <i class="fas fa-ban"></i> Cancelar
 </button>
 
-<button type="submit" class="btn btn-outline-danger ml-1" @include('partials.disabled',['model'=>$model])>
+<button
+    type="submit"
+    class="btn btn-outline-danger ml-1"
+    @include('partials.disabled', ['model' => $model ?? null])
+>
     <i class="fa fa-save"></i> Gravar
 </button>
