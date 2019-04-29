8d1bf47b689a3be96daeee473cd7175202f4b418
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2019-04-29 18:44:43 -0300

    Change button to input group

--- resources/views/layouts/partials/search-form.blade.php
+++ resources/views/layouts/partials/search-form.blade.php
@@ -23,18 +23,21 @@
                             <i class="fa fa-search"></i>
                         </span>
                     </div>
+
+                    @if(isset($routeSearch))
+                        <div class="input-group-append">
+                            <span
+                                class="input-group-text"
+                                onClick="javascript:document.getElementById('searchForm').submit();"
+                            >
+                                <a href="{{ route($routeSearch) }}">
+                                    <i class="fas fa-eraser"></i> Limpar
+                                </a>
+                            </span>
+                        </div>
+                    @endif
                 </div>
             </div>
-
-            @if(isset($routeSearch))
-                <div class="col-xs-8 d-flex justify-content-end">
-                    <div class="input-group">
-                        <a href="{{ route($routeSearch) }}" class="btn btn-primary pull-right">
-                            <i class="fas fa-eraser"></i> Limpar
-                        </a>
-                    </div>
-                </div>
-            @endif
         </div>
     </div>
 </form>
