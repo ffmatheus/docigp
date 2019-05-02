5ad4ccf06213e72a9a627f9999a6f8792a47b452
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2019-05-02 20:22:14 -0300

    Remove request from production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -37,6 +37,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        info(request()->all());
+        if (App::environment('local')) {
+            info(request()->all());
+        }
     }
 }
