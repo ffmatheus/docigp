77d9f049a8a82fc5dfcee7049b1379785218f7cb
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2019-07-04 16:01:04 -0300

    Change name and align things

--- resources/views/admin/home/index.blade.php
+++ resources/views/admin/home/index.blade.php
@@ -2,104 +2,98 @@
 
 @section('content')
     <div class="col-md-12" id="vue-admin">
-        <div class="card">
-            <div class="card-header">Painel de Controle</div>
-
-            <div class="card-body">
-                <div class="row">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    <div class="col-sm-12 col-md-6 col-lg-4">
-                        @include('partials.tile', [
-                            'route' => route('entries.index'),
-                            'title' => 'Prestação de contas',
-                            'color' => 'green',
-                            'icon' => 'fa fa-check-circle fa-5x'
-                        ])
-                    </div>
-
-                    @can('parties:show')
-                        <div class="col-sm-12 col-md-6 col-lg-4">
-                            @include('partials.tile', [
-                                'route' => route('parties.index'),
-                                'title' => 'Partidos',
-                                'color' => 'orange',
-                                'icon' => 'fa fa-handshake fa-5x'
-                            ])
-                        </div>
-                    @endCan
-
-                    @can('legislatures:show')
-                        <div class="col-sm-12 col-md-6 col-lg-4">
-                            @include('partials.tile', [
-                                'route' => route('legislatures.index'),
-                                'title' => 'Legislaturas',
-                                'color' => 'purple',
-                                'icon' => 'fa fa-calendar fa-5x'
-                            ])
-                        </div>
-                    @endCan
-
-                    @can('congressmen:show')
-                        <div class="col-sm-12 col-md-6 col-lg-4">
-                            @include('partials.tile', [
-                                'route' => route('congressmen.index'),
-                                'title' => 'Deputados',
-                                'color' => 'red',
-                                'icon' => 'fa fa-landmark fa-5x'
-                            ])
-                        </div>
-                    @endCan
-
-                    @can('users:show')
-                        <div class="col-sm-12 col-md-6 col-lg-4">
-                            @include('partials.tile', [
-                                'route' => route('users.index'),
-                                'title' => 'Usuários',
-                                'color' => 'blue',
-                                'icon' => 'fa fa-users fa-5x'
-                            ])
-                        </div>
-                    @endCan
-
-                    @can('providers:show')
-                        <div class="col-sm-12 col-md-6 col-lg-4">
-                            @include('partials.tile', [
-                                'route' => route('providers.index'),
-                                'title' => 'Fornecedores / Favorecidos',
-                                'color' => 'green',
-                                'icon' => 'fa fa-store fa-5x'
-                            ])
-                        </div>
-                    @endCan
-
-                    @can('cost-centers:show')
-                        <div class="col-sm-12 col-md-6 col-lg-4">
-                            @include('partials.tile', [
-                                'route' => route('cost-centers.index'),
-                                'title' => 'Centros de custo',
-                                'color' => 'purple',
-                                'icon' => 'fa fa-donate fa-5x'
-                            ])
-                        </div>
-                    @endCan
-
-                    @can('entry-types:show')
-                        <div class="col-sm-12 col-md-6 col-lg-4">
-                            @include('partials.tile', [
-                                'route' => route('entry-types.index'),
-                                'title' => 'Tipos de lançamento',
-                                'color' => 'red',
-                                'icon' => 'fa fa-clipboard fa-5x'
-                            ])
-                        </div>
-                    @endCan
+        <div class="row">
+            @if (session('status'))
+                <div class="alert alert-success" role="alert">
+                    {{ session('status') }}
                 </div>
+            @endif
+
+            <div class="col-sm-12 col-md-6 col-lg-4">
+                @include('partials.tile', [
+                    'route' => route('entries.index'),
+                    'title' => 'Prestação de contas',
+                    'color' => 'green',
+                    'icon' => 'fa fa-check-circle fa-5x'
+                ])
             </div>
+
+            @can('parties:show')
+                <div class="col-sm-12 col-md-6 col-lg-4">
+                    @include('partials.tile', [
+                        'route' => route('parties.index'),
+                        'title' => 'Partidos',
+                        'color' => 'orange',
+                        'icon' => 'fa fa-handshake fa-5x'
+                    ])
+                </div>
+            @endCan
+
+            @can('legislatures:show')
+                <div class="col-sm-12 col-md-6 col-lg-4">
+                    @include('partials.tile', [
+                        'route' => route('legislatures.index'),
+                        'title' => 'Legislaturas',
+                        'color' => 'purple',
+                        'icon' => 'fa fa-calendar fa-5x'
+                    ])
+                </div>
+            @endCan
+
+            @can('congressmen:show')
+                <div class="col-sm-12 col-md-6 col-lg-4">
+                    @include('partials.tile', [
+                        'route' => route('congressmen.index'),
+                        'title' => 'Deputados',
+                        'color' => 'red',
+                        'icon' => 'fa fa-landmark fa-5x'
+                    ])
+                </div>
+            @endCan
+
+            @can('users:show')
+                <div class="col-sm-12 col-md-6 col-lg-4">
+                    @include('partials.tile', [
+                        'route' => route('users.index'),
+                        'title' => 'Usuários',
+                        'color' => 'blue',
+                        'icon' => 'fa fa-users fa-5x'
+                    ])
+                </div>
+            @endCan
+
+            @can('providers:show')
+                <div class="col-sm-12 col-md-6 col-lg-4">
+                    @include('partials.tile', [
+                        'route' => route('providers.index'),
+                        'title' => 'Fornecedores / Favorecidos',
+                        'color' => 'green',
+                        'icon' => 'fa fa-store fa-5x'
+                    ])
+                </div>
+            @endCan
+
+            @can('cost-centers:show')
+                <div class="col-sm-12 col-md-6 col-lg-4">
+                    @include('partials.tile', [
+                        'route' => route('cost-centers.index'),
+                        'title' => 'Centros de custo',
+                        'color' => 'purple',
+                        'icon' => 'fa fa-donate fa-5x'
+                    ])
+                </div>
+            @endCan
+
+            @can('entry-types:show')
+                <div class="col-sm-12 col-md-6 col-lg-4">
+                    @include('partials.tile', [
+                        'route' => route('entry-types.index'),
+                        'title' => 'Tipos de lançamento',
+                        'color' => 'red',
+                        'icon' => 'fa fa-clipboard fa-5x'
+                    ])
+                </div>
+            @endCan
         </div>
     </div>
 @endsection
--- resources/views/layouts/partials/menu.blade.php
+++ resources/views/layouts/partials/menu.blade.php
@@ -1,105 +1,111 @@
 <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
     <div class="container-fluid">
-        <a class="navbar-brand" href="{{ url('/') }}">
-            {{ config('app.medium_name') }}
-        </a>
+        <div class="row">
+            <div class="col-8">
+                <a class="navbar-brand" href="{{ url('/') }}" style="white-space: normal;">
+                    {{ config('app.long_name') }}
+                </a>
+            </div>
 
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-            <span class="navbar-toggler-icon"></span>
-        </button>
+            <div class="col-4 my-auto text-right">
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
 
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <!-- Left Side Of Navbar -->
-            <ul class="navbar-nav mr-auto">
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <!-- Left Side Of Navbar -->
+                    <ul class="navbar-nav mr-auto">
 
-            </ul>
+                    </ul>
 
-            <!-- Right Side Of Navbar -->
-            <ul class="navbar-nav ml-auto">
-                <!-- Service Links -->
-                @guest
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                    </li>
-                @else
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('admin.index') }}">Painel de Controle</a>
-                    </li>
+                    <!-- Right Side Of Navbar -->
+                    <ul class="navbar-nav ml-auto">
+                        <!-- Service Links -->
+                        @guest
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                            </li>
+                        @else
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('admin.index') }}">Painel de Controle</a>
+                            </li>
 
-                    <li class="nav-item dropdown">
-                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                            Tabelas <span class="caret"></span>
-                        </a>
-
-                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                            <a class="dropdown-item" href="{{ route('entries.index') }}">
-                                Prestação de Contas
-                            </a>
-
-                            @can('parties:show')
-                                <a class="dropdown-item" href="{{ route('parties.index') }}">
-                                    Partidos
+                            <li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    Tabelas <span class="caret"></span>
                                 </a>
-                            @endCan
 
-                            @can('legislatures:show')
-                                <a class="dropdown-item" href="{{ route('legislatures.index') }}">
-                                    Legislaturas
+                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('entries.index') }}">
+                                        Prestação de Contas
+                                    </a>
+
+                                    @can('parties:show')
+                                        <a class="dropdown-item" href="{{ route('parties.index') }}">
+                                            Partidos
+                                        </a>
+                                    @endCan
+
+                                    @can('legislatures:show')
+                                        <a class="dropdown-item" href="{{ route('legislatures.index') }}">
+                                            Legislaturas
+                                        </a>
+                                    @endCan
+
+                                    @can('congressmen:show')
+                                        <a class="dropdown-item" href="{{ route('congressmen.index') }}">
+                                            Deputados
+                                        </a>
+                                    @endCan
+
+                                    @can('users:show')
+                                        <a class="dropdown-item" href="{{ route('users.index') }}">
+                                            Usuários
+                                        </a>
+                                    @endCan
+
+                                    @can('providers:show')
+                                        <a class="dropdown-item" href="{{ route('providers.index') }}">
+                                            Fornecedores / Favorecidos
+                                        </a>
+                                    @endCan
+
+                                    @can('cost-centers:show')
+                                        <a class="dropdown-item" href="{{ route('cost-centers.index') }}">
+                                            Centro de Custo
+                                        </a>
+                                    @endCan
+
+                                    @can('entry-types:show')
+                                        <a class="dropdown-item" href="{{ route('entry-types.index') }}">
+                                            Tipos de Lançamentos
+                                        </a>
+                                    @endCan
+
+                                </div>
+                            </li>
+
+                            <li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    {{ Auth::user()->name }} <span class="caret"></span>
                                 </a>
-                            @endCan
 
-                            @can('congressmen:show')
-                                <a class="dropdown-item" href="{{ route('congressmen.index') }}">
-                                    Deputados
-                                </a>
-                            @endCan
+                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                             document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    </a>
 
-                            @can('users:show')
-                                <a class="dropdown-item" href="{{ route('users.index') }}">
-                                    Usuários
-                                </a>
-                            @endCan
-
-                            @can('providers:show')
-                                <a class="dropdown-item" href="{{ route('providers.index') }}">
-                                    Fornecedores / Favorecidos
-                                </a>
-                            @endCan
-
-                            @can('cost-centers:show')
-                                <a class="dropdown-item" href="{{ route('cost-centers.index') }}">
-                                    Centro de Custo
-                                </a>
-                            @endCan
-
-                            @can('entry-types:show')
-                                <a class="dropdown-item" href="{{ route('entry-types.index') }}">
-                                    Tipos de Lançamentos
-                                </a>
-                            @endCan
-
-                        </div>
-                    </li>
-
-                    <li class="nav-item dropdown">
-                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                            {{ Auth::user()->name }} <span class="caret"></span>
-                        </a>
-
-                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                            <a class="dropdown-item" href="{{ route('logout') }}"
-                               onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                {{ __('Logout') }}
-                            </a>
-
-                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                @csrf
-                            </form>
-                        </div>
-                    </li>
-                @endguest
-            </ul>
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                        @csrf
+                                    </form>
+                                </div>
+                            </li>
+                        @endguest
+                    </ul>
+                </div>
+            </div>
         </div>
     </div>
 </nav>
