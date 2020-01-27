28b3566b9451fc1ce7a61e0746e5e1ebfa5e46a5
BMasquio <brunomasquio@gmail.com>
2020-01-27 17:11:04 -0300

    Fix User factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -6,24 +6,25 @@ use Illuminate\Support\Str;
 
 $factory->define(User::class, function () {
     do {
-        $email = faker()->unique()->safeEmail;
-    } while (app(UsersRepository::class)->findByEmail($email));
+        preg_match('/(.*?)@(.*)/', faker()->unique()->safeEmail, $output_array);
 
-    preg_match('/(.*?)@(.*)/', faker()->unique()->safeEmail, $output_array);
+        $username = $output_array[1];
+        $email = $output_array[1] . '@alerj.rj.gov.br';
+    } while (app(UsersRepository::class)->findByEmail($email));
 
     return [
         'name' => faker()->name,
-        'username' => $output_array[1],
-        'email' => $output_array[0],
+        'username' => $username,
+        'email' => $email,
         'email_verified_at' => now(),
         'password' =>
             '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-        'remember_token' => Str::random(10),
+        'remember_token' => Str::random(10)
     ];
 });
-$factory->defineAs(User::class, Constants::ROLE_ADMINISTRATOR, function ($faker) use (
-    $factory
-) {
+$factory->defineAs(User::class, Constants::ROLE_ADMINISTRATOR, function (
+    $faker
+) use ($factory) {
     $user = $factory->create(User::class);
     $user->assign(Constants::ROLE_ADMINISTRATOR);
     return $user->toArray();
