4e177a4634c95889df9a20f01d2539fcc1b41852
BMasquio <brunomasquio@gmail.com>
2020-06-24 18:59:02 +0000

    Fix

--- resources/views/admin/home/index.blade.php
+++ resources/views/admin/home/index.blade.php
@@ -99,7 +99,7 @@
                                             <div class="col-sm-12 col-md-6 col-lg-4">
                                                 @include('partials.tile', [
                                                 'route' => route('audits.index'),
-                                                'title' => 'Auditoria',
+                                                'title' => 'Auditorias',
                                                 'color' => 'blue',
                                                 'icon' => 'fas fa-user-tie fa-5x',
                                                 ])
